0e7937032a8eab93f487ebd67bd6a653abe41d29
Stepan <bai_777@mail.ru>
2021-07-17 20:48:25 +0300

    Сделал первое задание

--- app/src/main/java/com/example/dialogs/MainActivity.java
+++ app/src/main/java/com/example/dialogs/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.dialogs;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAlertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initBtns();
+    }
+
+    private void initBtns() {
+        btnAlertDialog = findViewById(R.id.btn_warning_from_delete);
+        btnAlertDialog.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_warning_from_delete:
+                // Создаём билдер и передаём контекст приложения
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                // В билдере указываем заголовок окна. Можно указывать как ресурс,
+                // так и строку
+                builder.setTitle(R.string.exclamation)
+                        .setMessage(R.string.press_button)
+                        .setIcon(R.mipmap.ic_launcher_round)
+                        // Из этого окна нельзя выйти кнопкой Back
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.btn_delete, new DialogInterface.OnClickListener() {
+                            // Ставим слушатель, нажатие будем обрабатывать
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "Файл удален!!!", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton(R.string.btn_cancellation, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
+                        .setNeutralButton(R.string.btn_cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                ;
+
+                AlertDialog alert = builder.create();
+                alert.show();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v);
+        }
     }
 }
